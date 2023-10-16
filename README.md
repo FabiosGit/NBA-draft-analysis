e74616bd8a32fb7fbfccb4fead1c50c151773450
FabiosGit <145550943+FabiosGit@users.noreply.github.com>
2023-10-16 14:27:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nba-draft-analysis
+ NBA draft analysis - Which NBA team selects the best players relative to the average player at their respective draft position?
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NBA draft analysis - Which NBA team selects the best players relative to the average player at their respective draft position?',
+    author='Luis Knufinke, Ralf Wyss, Fabio Nadaia',
+    license='MIT',
+)
